68565bc567ce196e8f862faaff9907106d59244d
SibahleMvubu <smvubu@iqbusiness.net>
2025-07-24 02:59:49 +0200

    npm-i

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'  // <-- important, add this!
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),  // map '@' to './src'
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    chunkSizeWarningLimit: 2000,  // optional, to silence warnings about chunk size
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react'
-            return 'vendor'
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "vendor-react";
+            return "vendor";
           }
         },
       },
     },
   },
-})
+});
