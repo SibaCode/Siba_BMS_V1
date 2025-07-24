5e705825c5ba0ab32028fdd33b017b658f8e1de0
SibahleMvubu <smvubu@iqbusiness.net>
2025-07-24 02:34:51 +0200

    CardContent

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'  // <-- important, add this!
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),  // map '@' to './src'
     },
   },
-}));
+  build: {
+    chunkSizeWarningLimit: 2000,  // optional, to silence warnings about chunk size
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react'
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
+})
